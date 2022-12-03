bb3cda2a3795abc87f0736927b3234a54000d355
Ryan Pratama <ru.ryanpratama@gmail.com>
2022-12-04 00:16:02 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { React, useEffect, useState, useRef } from "react";
 import toast, { Toaster } from "react-hot-toast";
 import { CountryDropdown, RegionDropdown } from "react-country-region-selector";
 // import Navbar from "./Navbar";
-// import Footer from "./Footer";
+import Footer from "./Footer";
 
 function App() {
   const [word, setWord] = useState("*Please fill all the fields");
@@ -12,6 +12,8 @@ function App() {
   const [lName, setLName] = useState("");
   const [telephoneNum, setTelephoneNum] = useState("");
   const [email, setEmail] = useState("");
+  const [age, setAge] = useState("");
+  const [instagram, setInstagram] = useState("");
   const [country, setCountry] = useState("");
   const [region, setRegion] = useState("");
   const [address, setAddress] = useState("");
@@ -26,6 +28,8 @@ function App() {
     if (
       fName.length > 0 &&
       lName.length > 0 &&
+      age.length === 2 &&
+      instagram.length > 0 &&
       telephoneNum.length > 0 &&
       email.match(validRegex) &&
       country.length > 0 &&
@@ -45,6 +49,8 @@ function App() {
     lName,
     telephoneNum,
     email,
+    age,
+    instagram,
     country,
     region,
     address,
@@ -61,10 +67,12 @@ function App() {
           <h2 className="text-2xl md:text-4xl xl:text-5xl font-bold tracking-tight mb-8 mt-8">
             Make a major life change,
             <br />
+            <span className="text-blue-300">join FaoTech, </span>
             <span className="text-red-300">NOW.</span>
           </h2>
         </div>
-        <div>
+
+        {/* <div>
           <div class="grid gap-6 mb-6 md:grid-cols-2">
             <div>
               <label
@@ -90,7 +98,7 @@ function App() {
               <input
                 type="text"
                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="McTominay"
+                placeholder="Pratama"
                 required
                 onChange={(e) => setLName(e.target.value)}
               />
@@ -178,10 +186,141 @@ function App() {
           </button>
 
           <p class="text-red-200 text-xs mt-5 mb-5">{word}</p>
+        </div> */}
+
+        <div>
+          <div class="grid gap-6 mb-6 md:grid-cols-2">
+            <div>
+              <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                First name
+              </label>
+              <input
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Ryan"
+                required
+                onChange={(e) => setFName(e.target.value)}
+              />
+            </div>
+            <div>
+              <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                Last name
+              </label>
+              <input
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Pratama"
+                required
+                onChange={(e) => setLName(e.target.value)}
+              />
+            </div>
+            <div>
+              <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                Age
+              </label>
+              <input
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="18+"
+                required
+                onChange={(e) => setAge(e.target.value)}
+              />
+            </div>
+            <div>
+              <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                Phone Number
+              </label>
+              <input
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="123-45-678"
+                required
+                onChange={(e) => setTelephoneNum(e.target.value)}
+              />
+            </div>
+          </div>
+          <div class="mb-6">
+            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+              Instagram Username
+            </label>
+            <input
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="@ryanpratama14"
+              required
+              onChange={(e) => setInstagram(e.target.value)}
+            />
+          </div>
+          <div class="mb-6">
+            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+              Email address
+            </label>
+            <input
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="shiritainouka@faotech.com"
+              required
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+
+          <div class="grid gap-6 mb-6 md:grid-cols-2">
+            <div>
+              <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                Country
+              </label>
+
+              <CountryDropdown
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                value={country}
+                onChange={(val) => setCountry(val)}
+              />
+            </div>
+            <div>
+              <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                Region
+              </label>
+
+              <RegionDropdown
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                country={country}
+                value={region}
+                onChange={(val) => setRegion(val)}
+              />
+            </div>
+          </div>
+
+          <div class="mb-6">
+            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+              Address
+            </label>
+            <input
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Elvie Street 91"
+              required
+              onChange={(e) => setAddress(e.target.value)}
+            />
+          </div>
+
+          <button
+            onClick={(event) => {
+              isSubmitted = isSubmitted + 1;
+              if (isFilledAll.current === true && isSubmitted === 1) {
+                notifySuccess();
+              } else if (isFilledAll.current === true && isSubmitted === 2) {
+                toast("Already submitted, thank you", {
+                  icon: "🙌",
+                });
+              } else if (isFilledAll.current === true && isSubmitted > 2) {
+                event.preventDefault();
+              } else {
+                notifyError();
+              }
+            }}
+            type="submit"
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Submit
+          </button>
         </div>
+        <p class="text-red-200 text-xs mt-5 mb-5">{word}</p>
       </div>
 
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer class="p-4 bg-gray-900 solid-lg shadow md:flex md:items-center md:justify-between md:p-6">
+      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+        © 2022{" "}
+        <a href="#" class="hover:underline">
+          FaoTech™
+        </a>
+        . All Rights Reserved.
+      </span>
+    </footer>
+  );
+};
+
+export default Footer;
