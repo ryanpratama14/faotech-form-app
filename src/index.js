c48ef8e0d2a8f1e83b4d3166a500a0b8842f608b
Ryan Pratama <ru.ryanpratama@gmail.com>
2022-12-05 01:14:00 +0300

    new

--- src/index.js
+++ src/index.js
@@ -2,16 +2,42 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import Navbar from "./pages/Navbar";
+import Footer from "./pages/Footer";
+import Testimonial from "./pages/Testimonial";
+// import reportWebVitals from "./reportWebVitals";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Cards from "./pages/Cards";
+import Appform from "./pages/Appform";
+import Zikirapp from "./pages/Zikirapp";
+import Declare from "./pages/Declare";
+
+export default function Routing() {
+  return (
+    <HashRouter>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<App />} />
+        <Route path="/zikir-app" element={<Zikirapp />} />
+        <Route path="/zikir-app/declare" element={<Declare />} />
+        <Route path="/reviews" element={<Testimonial />} />
+        <Route path="/cards" element={<Cards />} />
+        <Route path="/appform" element={<Appform />} />
+        <Route path="*" element={<App />} />
+      </Routes>
+      <Footer />
+    </HashRouter>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Routing />
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
